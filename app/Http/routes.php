3cfab80db1e8c630f25deeed6c4ff872ae75a743
Intern <intern@InternnoMacBook-Air.local>
2018-04-27 12:51:17 +0900

    [add] MainController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/index', function () {
-    return view('index');
-});
+Route::get('/','MainController@index');
