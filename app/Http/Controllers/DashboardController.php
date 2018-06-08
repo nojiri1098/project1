9717e69b1e1629eadbfb34e0483298938c5f27f7
Daiki <nojiri1098@gmail.com>
2018-06-08 11:28:15 +0900

    [add] openWeatherMapで天気を取得する場合のコード

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -29,20 +29,20 @@ class DashboardController extends Controller
         $temp    = 20;
         $hum     = 50;
         $co2     = 1000;
-        $weather = 1;
+        $weather = $this->getOpenWeatherMap();
         $rain    = 50;
 
         $env = new Environment();
         $env->temperature = $temp;
         $env->humidity = $hum;
         $env->co2 = $co2;
-        $env->weather = $weather;
+        $env->weather = $weather['weather'][0]['main'];
         $env->rain = $rain;
         $env->save();
 
         $planter_id = [1,2,3,4];
         $water = [1000,2000,1200,1234];
-
+        
         for ($i = 0; $i < count($planter_id); $i++) {
             $soil = new Soil();
             $soil->planter_id = $planter_id[$i];
@@ -54,15 +54,23 @@ class DashboardController extends Controller
         return redirect()->route('dashboard')->with(['success', '成功']);
     }
 
-    public function getWeather()
+    public function getOpenWeatherMap()
     {
+//        WEATHER_URL=http://api.openweathermap.org/data/2.5/weather
+//        WEATHER_API=87de68884044e8e9a266d3e300cc8692
+//        WEATHER_LOCATION=Oita-shi,jp
         $baseUrl = env('WEATHER_URL');
         $api = env('WEATHER_API');
         $location = env('WEATHER_LOCATION');
         $url = $baseUrl . '?q=' . $location . '&units=metric&appid=' . $api;
 
         $weather = json_decode(file_get_contents($url), true);
-        
+        dd($weather);
+        $data['weahter'] = $weather['weather'][0]['main'];
+        $data['temp_max'] = $weather['main']['temp_max'];
+        $data['temp_min'] = $weather['main']['temp_min'];
+        $data['humidity'] = $weather['main']['humidity'];
+
         return redirect()->route('dashboard')->with(['success', '成功']);
     }
 }
