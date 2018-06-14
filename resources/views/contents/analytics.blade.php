b9e4df5932e79c949e3c087f49891593915cb52e
Daiki <nojiri1098@gmail.com>
2018-06-14 18:26:40 +0900

    [modify] dashboard，data，analyticsをAdminLTE3に対応させた

--- resources/views/contents/analytics.blade.php
+++ resources/views/contents/analytics.blade.php
@@ -1,23 +1,36 @@
 @extends('layouts.app')
 
-@section('title')
-    AdminLTE 2 | Analytics
-@stop
-
 @section('content')
 
-    <section class="content-header">
-        <h1>
-            Analytics
-            <small>Data analytics</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Analytics</a></li>
-            <li class="active">Analytics</li>
-        </ol>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Dashboard</h1>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v2</li>
+                        </ol>
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
 
-        <a href={{ route('store') }}>store</a>
-
-    </section>
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+            </div>
+        </section>
+    </div>
 
 @stop
--- resources/views/contents/data.blade.php
+++ resources/views/contents/data.blade.php
@@ -1,98 +1,82 @@
 @extends('layouts.app')
 
-@section('title')
-    AdminLTE 2 | Data
-@stop
-
 @section('content')
 
-    <section class="content-header">
-        <h1>
-            Data
-            <small>Detail data</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Data</a></li>
-            <li class="active">Data</li>
-        </ol>
-    </section>
-
-    <section class="content">
-        <div class="row">
-
-            <section class="col-lg-5 connectedSortable">
-                <!-- Calendar -->
-                <div class="box box-solid">
-                    <div class="box-header">
-                        <i class="fa fa-calendar"></i>
-
-                        <h3 class="box-title">Calendar</h3>
-                        <!-- tools box -->
-                        <div class="pull-right box-tools">
-                            <!-- button with a dropdown -->
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown">
-                                    <i class="fa fa-bars"></i></button>
-                                <ul class="dropdown-menu pull-right" role="menu">
-                                    <li><a href="#">Add new event</a></li>
-                                    <li><a href="#">Clear events</a></li>
-                                    <li class="divider"></li>
-                                    <li><a href="#">View calendar</a></li>
-                                </ul>
-                            </div>
-                            <button type="button" class="btn btn-success btn-sm" data-widget="collapse"><i class="fa fa-minus"></i>
-                            </button>
-                            <button type="button" class="btn btn-success btn-sm" data-widget="remove"><i class="fa fa-times"></i>
-                            </button>
-                        </div>
-                        <!-- /. tools -->
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">Dashboard</h1>
                     </div>
-                    <!-- /.box-header -->
-                    <div class="box-body no-padding">
-                        <!--The calendar -->
-                        <div id="calendar" style="width: 100%"></div>
+                    <!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v2</li>
+                        </ol>
                     </div>
-                    <!-- /.box-body -->
+                    <!-- /.col -->
                 </div>
-                <!-- /.box -->
-            </section>
-
-            <div class="col-xs-12">
-                <div class="box">
-                    <div class="box-header">
-                        <h3 class="box-title">Data Table</h3>
-                    </div>
-                    <div class="box-body">
-                        <table id="example2" class="table table-bordered table-hover">
-                            <thead>
-                            <tr>
-                                <th>Temperature</th>
-                                <th>Humidity</th>
-                                <th>Co2</th>
-                                <th>Weather</th>
-                                <th>Rain Percentage</th>
-                                <th>Water 1</th>
-                                <th>Water 2</th>
-                                <th>Water 3</th>
-                                <th>Water 4</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($envs as $env)
-                                <tr>
-                                    <td>{{ $env->temperature }}</td>
-                                    <td>{{ $env->humidity }}</td>
-                                    <td>{{ $env->co2 }}</td>
-                                    <td>{{ $env->weather }}</td>
-                                    <td>{{ $env->rain }}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <!-- /.row -->
             </div>
+            <!-- /.container-fluid -->
         </div>
-    </section>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">Responsive Hover Table</h3>
+
+                                <div class="card-tools">
+                                    <div class="input-group input-group-sm" style="width: 150px;">
+                                        <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+
+                                        <div class="input-group-append">
+                                            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body table-responsive p-0">
+                                <table class="table table-hover">
+                                    <tr>
+                                        <th>Temperature</th>
+                                        <th>Humidity</th>
+                                        <th>Co2</th>
+                                        <th>Weather</th>
+                                        <th>Rain Percentage</th>
+                                        <th>Water 1</th>
+                                        <th>Water 2</th>
+                                        <th>Water 3</th>
+                                        <th>Water 4</th>
+                                    </tr>
+                                    @foreach($envs as $env)
+                                        <tr>
+                                            <td>{{ $env->temperature }}</td>
+                                            <td>{{ $env->humidity }}</td>
+                                            <td>{{ $env->co2 }}</td>
+                                            <td>{{ $env->weather }}</td>
+                                            <td>{{ $env->rain }}</td>
+                                        </tr>
+                                    @endforeach
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                    </div>
+                </div><!-- /.row -->
+            </div>
+        </section>
+    </div>
 
 @stop
