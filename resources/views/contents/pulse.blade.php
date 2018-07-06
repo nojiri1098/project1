2146e5005147017c5908c8356e12d11410073290
takuho-kudo <1005takuho@gmail.com>
2018-07-06 10:06:09 +0900

    [remove] フォームを削除

--- resources/views/contents/pulse.blade.php
+++ resources/views/contents/pulse.blade.php
@@ -11,7 +11,6 @@
                     <div class="col-sm-6">
                         <h1>pulse</h1>
                     </div>
-                    <input type="text">
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
