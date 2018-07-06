8927fc595e5b61d42e3c0e04fae527aeabf62485
takuho-kudo <1005takuho@gmail.com>
2018-07-06 10:05:09 +0900

    [add] フォームを追加

--- resources/views/contents/pulse.blade.php
+++ resources/views/contents/pulse.blade.php
@@ -11,6 +11,7 @@
                     <div class="col-sm-6">
                         <h1>pulse</h1>
                     </div>
+                    <input type="text">
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
