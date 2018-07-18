33e3f92c503a794e545c91b8bc6f743d25bb1ce0
Daiki <nojiri1098@gmail.com>
2018-07-18 17:36:35 +0900

    no message

--- public/lib/GetValue.py
+++ public/lib/GetValue.py
@@ -17,35 +17,37 @@ def main():
     args = sys.argv
     try :
         flag = args[1] + ',' + args[2] + ',' + args[3]
+        flag2 = flag.encode('utf-8')
+
+        ser.write(flag2)
+        ser.close()
     except:
         flag = args[1]
+        flag2 = flag.encode('utf-8')
 
-    flag2 = flag.encode('utf-8')
+        num = 0
+        while num < 3:
+            ser.write(flag2)
+            sleep(1)
 
-    num = 0
-    while num < 3:
-        #文字列送信
-        ser.write(flag2)
-        sleep(1)
+            line = ser.readline()
+            line2 = line.rstrip().decode('utf-8')
 
-        line = ser.readline()
-        line2 = line.rstrip().decode('utf-8')
+            try :
+                all_sensors = line2.split(',')
+            except:
+                all_sensors = "取得がうまくいかなかった"
 
-        try :
-            all_sensors = line2.split(',')
-        except:
-            all_sensors = "取得がうまくいかなかった"
+            num += 1
+        
+        print(all_sensors[0])
+        print(all_sensors[1])
+        print(all_sensors[3])
+        print(all_sensors[2])
+        print(all_sensors[4])
+        print(all_sensors[5])
 
-        num += 1
-
-    print(all_sensors[0])
-    print(all_sensors[1])
-    print(all_sensors[3])
-    print(all_sensors[2])
-    print(all_sensors[4])
-    print(all_sensors[5])
-
-    ser.close()
+        ser.close()
 
 if __name__ == '__main__':
     main()
