62b9dc41dac96c3ea33e30e8b112324409a2c93d
Daiki <nojiri1098@gmail.com>
2018-07-18 18:09:09 +0900

    [add] try-except

--- public/lib/GetValue.py
+++ public/lib/GetValue.py
@@ -9,7 +9,7 @@ def main():
     # COMポートを指定してシリアル通信開始
     # /dev/ttyACM0
     # /dev/usbmodem1421
-    ser = serial.Serial('/dev/tty.usbmodem1411',9600,timeout=1)
+    ser = serial.Serial('/dev/ttyACM0',9600,timeout=1)
 
     # 引数
     # 第1引数 : args[1]
@@ -39,13 +39,15 @@ def main():
                 all_sensors = "取得がうまくいかなかった"
 
             num += 1
-        
-        print(all_sensors[0])
-        print(all_sensors[1])
-        print(all_sensors[3])
-        print(all_sensors[2])
-        print(all_sensors[4])
-        print(all_sensors[5])
+        try:
+            print(all_sensors[0])
+            print(all_sensors[1])
+            print(all_sensors[3])
+            print(all_sensors[2])
+            print(all_sensors[4])
+            print(all_sensors[5])
+        except:
+            print("取得がうまくいかなかった")
 
         ser.close()
 
