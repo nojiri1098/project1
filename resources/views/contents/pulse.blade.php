91a4d933adc006d8a591156b04c433b6e90348ad
takuho-kudo <1005takuho@gmail.com>
2018-07-19 09:58:48 +0900

    [modify]パルスページのレイアウト

--- resources/views/contents/pulse.blade.php
+++ resources/views/contents/pulse.blade.php
@@ -24,7 +24,7 @@
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-md-8 mx-auto">
                             <div class="card">
                                 <div class="card-header no-border">
                                     <div class="d-flex justify-content-between">
